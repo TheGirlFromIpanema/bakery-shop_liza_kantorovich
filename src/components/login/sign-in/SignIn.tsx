e56a2335f74062d60a8a9c6e918ae80581a8cd5d
userCheck <eopolchentseva@gmail.com>
2025-06-19 17:37:48 +0300

    Fixed warnings

--- src/components/login/sign-in/SignIn.tsx
+++ src/components/login/sign-in/SignIn.tsx
@@ -195,7 +195,7 @@ export default function SignIn(props: { disableCustomTheme?: boolean, func: (obj
             <Typography sx={{ textAlign: 'center' }}>
               Don&apos;t have an account?{' '}
               <Link
-                href="/material-ui/getting-started/templates/sign-in/"
+                // href="/material-ui/getting-started/templates/sign-in/"
                 variant="body2"
                 sx={{ alignSelf: 'center' }}
               >
