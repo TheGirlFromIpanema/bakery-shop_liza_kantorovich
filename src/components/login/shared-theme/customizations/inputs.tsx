d6afa0ca317aadd38912c3d8c545f0919ecdc1ad
userCheck <eopolchentseva@gmail.com>
2025-06-19 17:41:59 +0300

    Fixed warnings

--- src/components/login/shared-theme/customizations/inputs.tsx
+++ src/components/login/shared-theme/customizations/inputs.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+
 import { alpha, Theme, Components } from '@mui/material/styles';
 import { outlinedInputClasses } from '@mui/material/OutlinedInput';
 import { svgIconClasses } from '@mui/material/SvgIcon';
@@ -9,7 +9,7 @@ import CheckRoundedIcon from '@mui/icons-material/CheckRounded';
 import RemoveRoundedIcon from '@mui/icons-material/RemoveRounded';
 import { gray, brand } from '../themePrimitives';
 
-/* eslint-disable import/prefer-default-export */
+
 export const inputsCustomizations: Components<Theme> = {
   MuiButtonBase: {
     defaultProps: {
