29e2aecba51621a7607b42ccb91e32759f8846ce
userCheck <eopolchentseva@gmail.com>
2025-06-24 21:01:43 +0300

    firestore activated. start to create DB service

--- src/utils/shop-types.ts
+++ src/utils/shop-types.ts
@@ -34,3 +34,12 @@ export type RegisterData = {
     password: string,
     name: string,
 }
+
+export type ProductType = {
+    id?: string,
+    title: string,
+    category: string,
+    unit: string,
+    cost: number,
+    img: string,
+}
